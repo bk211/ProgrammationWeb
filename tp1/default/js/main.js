e0a4fb8b5e2b7fac3951b72e7b55af5a262bd843
playmaker <chaoleicai@gmail.com>
2019-10-03 14:46:51 +0000

    affichage image entier on click

--- tp1/default/js/main.js
+++ tp1/default/js/main.js
@@ -8,28 +8,38 @@ const main = event => {
 	console.log("MAIN");
 
 //		let component = new MyComponent("canvas");
-		myElement = document.createElement("canvas");
-		document.body.appendChild(myElement);
-		myElement.style.width= "100%";
-		myElement.style.height= "100%";
+		canvas = document.createElement("canvas");
+		document.body.appendChild(canvas);
+		canvas.width= 1920;
+		canvas.height= 1080;
 
-		myElement.style.backgroundColor= "blue";
-		console.log("2");
-		ctx = myElement.getContext("2d");
+		canvas.style.width= "900px";
+		canvas.style.height= "600px";
+
+		canvas.style.backgroundColor= "blue";
+		ctx = canvas.getContext("2d");
 		img = new Image();
-		img.src = "img/car.jpg";
-		ctx.drawImage(img,0,0);
-		myElement.addEventListener("click", onClick);
-	//	document.body.appendChild(img);
+		img.src = "img/hibou.jpg";
+		img.style.width = "900px";
+		img.style.height = "600px";
+		document.body.appendChild(img);
+		canvas.addEventListener("click", onClick);
 
 }
 
 const onClick = event => {
 	console.log("clicked");
+	let cx = window.innerHeight;
+	let cy = window.innerWidth;
+	//console.log(canvas.width); // 1920
+	//console.log(canvas.height); // 1080
+
+	console.log(cx);
+	console.log(cy);
 	console.log(event.clientX);
 	console.log(event.clientY);
 
-	ctx.drawImage(img,event.clientX, event.clientY,100,100,event.clientX, event.clientY,100,100);
+	ctx.drawImage(img,0,0, 1900, 1080, 0,0,canvas.width,canvas.height);
 
 //	console.log(MM.randRange(0,5));
 
