47f00fbf6b2e2ce1e7c6c4332645f14d8a489d11
playmaker <chaoleicai@gmail.com>
2019-10-30 17:35:02 +0000

    add posttest

--- tp4/js/main.js
+++ tp4/js/main.js
@@ -2,9 +2,24 @@ window.addEventListener("load", event => {main()});
 
 const main = async () => {
     console.log("MAIN");
+
+    let post = "nnnnn"
     let result = await fetch('https://ilusio.dev/base/gettest');
     let response = await result.json();
-
     console.log(result);
     console.log(response);
+
+    let data = {number: 5};
+    let send = Object.keys(data).map(key => encodeURIComponent(key) +
+    "=" + encodeURIComponent(data[key])).join("&");
+
+    result = await fetch('https://ilusio.dev/base/posttest', {
+        method: "POST",
+        headers: {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"},
+        body:send});
+
+    response = await result.json();
+    console.log(response);
+
+
 }
